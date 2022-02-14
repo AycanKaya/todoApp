6d4840016e2789f915e6346b4e29bae9a6896f9e
Aycan <aycnkya12@gmail.com>
2022-02-14 17:44:48 +0300

    First Commit

---
+++ Controller/home.php
@@ -0,0 +1,9 @@
+<?php
+
+if(route(0)=='home'){
+
+    view('home/home',[
+        'isim'=>'Aycan',
+        'soyisim'=>'Kaya'
+    ]);
+}
---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+const BASEDIR='C:\xampp\htdocs\todoApp';
+const URL='http://localhost/todoApp/';
+const DEV_MOD=true;
